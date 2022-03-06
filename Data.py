e83ee7fd088abef93fc8112ebc68777068c3075f
SUSHILxPLAYER <100741130+SUSHILxPLAYER@users.noreply.github.com>
2022-03-06 07:21:25 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -10,7 +10,7 @@ Welcome to {}
 
 𝗬𝗼𝘂 𝗖𝗮𝗻 𝗨𝘀𝗲 𝗠𝗲 𝗧𝗼 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲 𝗣𝘆𝗿𝗼𝗴𝗿𝗮𝗺 𝗔𝗻𝗱 𝗧𝗲𝗹𝗲𝘁𝗵𝗼𝗻 𝗦𝘁𝗿𝗶𝗻𝗴 𝗦𝗲𝘀𝘀𝗶𝗼𝗻. 𝗨𝘀𝗲 𝗕𝗲𝗹𝗼𝘄 𝗕𝘂𝘁𝘁𝗼𝗻𝘀 𝗧𝗼 𝗟𝗲𝗮𝗿𝗻 𝗠𝗼𝗿𝗲 !
 
-By @JaiHindChatting
+By @TAG_FBAN
     """
 
     # Home Button
@@ -27,7 +27,7 @@ By @JaiHindChatting
     buttons = [
         [InlineKeyboardButton("🔥 𝗦𝘁𝗮𝗿𝘁 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗶𝗻𝗴 𝗦𝗲𝘀𝘀𝗶𝗼𝗻 🔥", callback_data="generate")],
         [InlineKeyboardButton("🤔 𝗛𝗼𝘄 𝗧𝗼 𝗨𝘀𝗲 𝗠𝗲 🤔", callback_data="help")],
-        [InlineKeyboardButton("♥ 𝗦𝗼𝘂𝗿𝗰𝗲 𝗔𝗻𝗱 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ♥", url="https://t.me/JaiHindChatting")],
+        [InlineKeyboardButton("♥ 𝗦𝗼𝘂𝗿𝗰𝗲 𝗔𝗻𝗱 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ♥", url="https://t.me/TAG_FBAN")],
     ]
 
     # Help Message
